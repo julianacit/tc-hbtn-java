2ee5f8242d9c2c70c7d8173d760630425a179914
julianacit <julianamt@ciandt.com>
2022-05-16 13:42:28 -0300

    mudando currency para trocar . e ,

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -4,8 +4,7 @@ public class PrintFormatacaoNumeros {
     public static void main (String args[]) {
         float taxa = 0.2456f;
         float valor = 7654.321f;
-        Locale locale = new Locale("pt", "BR");
-        NumberFormat formatValor = NumberFormat.getInstance(locale);
+        NumberFormat formatValor = NumberFormat.getInstance(new Locale("da", "DK"));
         formatValor.setMaximumFractionDigits(2);
         System.out.printf("Valor: R$ %s%n", formatValor.format(valor));
         System.out.printf("Taxa: %.2f%%%n",taxa);
