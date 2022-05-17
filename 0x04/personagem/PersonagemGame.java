0848bd9f252c1ef05458f2e3e12bf3f6827b0cf2
julianacit <julianamt@ciandt.com>
2022-05-17 12:31:51 -0300

    PersonagemGame

---
+++ 0x04/personagem/PersonagemGame.java
@@ -0,0 +1,20 @@
+public class PersonagemGame {
+    public int saudeAtual;
+    public String nome;
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+    }
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
