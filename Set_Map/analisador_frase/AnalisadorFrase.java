2e63c916c545299f02b96a21005f850678051ae1
julianacit <julianamt@ciandt.com>
2022-06-01 17:13:40 -0300

    mantendo as virgulas

--- Set_Map/analisador_frase/AnalisadorFrase.java
+++ Set_Map/analisador_frase/AnalisadorFrase.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class AnalisadorFrase {
     public static TreeMap<String, Integer> contagemPalavras(String frase) {
         TreeMap<String, Integer> contagem = new TreeMap<String, Integer>();
-        frase = frase.toLowerCase().replaceAll("\\?","").replaceAll("\\.", "").replaceAll("\\!", "").replaceAll("\\,", "").replaceAll("  ", " ");
+        frase = frase.toLowerCase().replaceAll("\\?","").replaceAll("\\.", "").replaceAll("\\!", "").replaceAll("  ", " ");
         List<String> palavras = new ArrayList<String>(List.of(frase.split(" ")));
 
         for (String palavra: palavras) {
