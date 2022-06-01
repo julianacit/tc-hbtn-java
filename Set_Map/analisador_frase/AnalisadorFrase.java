6ece2bec1f2928512c12dddf8706b1f7f0f606b1
julianacit <julianamt@ciandt.com>
2022-06-01 17:12:00 -0300

    Task 00 - set map analisador frase

---
+++ Set_Map/analisador_frase/AnalisadorFrase.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class AnalisadorFrase {
+    public static TreeMap<String, Integer> contagemPalavras(String frase) {
+        TreeMap<String, Integer> contagem = new TreeMap<String, Integer>();
+        frase = frase.toLowerCase().replaceAll("\\?","").replaceAll("\\.", "").replaceAll("\\!", "").replaceAll("\\,", "").replaceAll("  ", " ");
+        List<String> palavras = new ArrayList<String>(List.of(frase.split(" ")));
+
+        for (String palavra: palavras) {
+            if (contagem.containsKey(palavra))
+                contagem.put(palavra, contagem.get(palavra)+1);
+            else
+                contagem.put(palavra, 1);
+        }
+
+        return contagem;
+    }
+}
