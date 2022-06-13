6109e02bd44fb0e0ecaf92e865de40577d87df30
julianacit <julianamt@ciandt.com>
2022-06-13 15:15:33 -0300

    Task 01 - streams

---
+++ Streams_Collectors_Optional/maior_preco/Consulta.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class Consulta {
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        return pedido.getProdutos().stream().filter(produto -> produto.getCategoria() == CategoriaProduto.LIVRO).collect(Collectors.toList());
+    }
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+        return produtos.stream().sorted(Comparator.comparing(Produto::getPreco).reversed()).findFirst().get();
+    }
+}
