106bdd0edc89a03f73ebceffef229db7f05805af
julianacit <julianamt@ciandt.com>
2022-06-13 14:49:18 -0300

    Task 00 - streams

---
+++ Streams_Collectors_Optional/livros/Consulta.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class Consulta {
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        return pedido.getProdutos().stream().filter(produto -> produto.getCategoria() == CategoriaProduto.LIVRO).collect(Collectors.toList());
+    }
+}
