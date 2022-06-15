1fafd0b97f68848d314a78a5ae53b2c6a7669546
julianacit <julianamt@ciandt.com>
2022-06-15 18:18:29 -0300

    -

--- apache-maven/financeiro-api/src/main/java/com/techcamps/financeiro/FinanceiroMain.java
+++ apache-maven/financeiro-api/src/main/java/com/techcamps/financeiro/FinanceiroMain.java
@@ -1,6 +1,5 @@
 package com.techcamps.financeiro;
-public class App 
-{
+public class FinanceiroMain{
     public static void main( String[] args )
     {
         System.out.println( "Criando um projeto Java Maven." );
